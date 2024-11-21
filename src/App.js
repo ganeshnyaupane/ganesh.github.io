532f9c56fbea24817369c3301b253d2541f3c57f
Ganesh <ganesh@Ganeshs-Air.attlocal.net>
2024-11-20 23:12:30 -0800

    Update react from router v5 to router v6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import
-{
-BrowserRouter as Router,
-Route,
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
 } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './contents/Home';
@@ -12,34 +12,32 @@ import Education from './contents/Education';
 import Skills from './contents/Skills';
 import Contact from './contents/Contact';
 import Projects from './contents/Projects';
+
 function App() {
-return (
-<Router>
-<div className="App">
-<Navbar />
-{/* Route for Home.js contents */}
-<Route exact path="/">
-<Home />
-</Route>
-{/* Route for About.js contents */}
-<Route path="/about">
-<About />
-</Route>
-{/* Route for Education.js contents */}
-<Route path="/education">
-<Education />
-</Route>
-<Route path="/skills">
-<Skills/>
-</Route>
-<Route path="/projects">
-<Projects/>
-</Route>
-<Route path="/contact">
-<Contact/>
-</Route>
-</div>
-</Router>
-)
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        {/* Wrap Routes inside Routes component */}
+        <Routes>
+          {/* Route for Home.js contents */}
+          <Route path="/" element={<Home />} />
+          
+          {/* Route for About.js contents */}
+          <Route path="/about" element={<About />} />
+          
+          {/* Route for Education.js contents */}
+          <Route path="/education" element={<Education />} />
+          
+          <Route path="/skills" element={<Skills />} />
+          
+          <Route path="/projects" element={<Projects />} />
+          
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
+
 export default App;
